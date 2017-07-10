434a5de312bc9d9a759d89dc0640ec0b7c03d158
j-ew-s <gabriel.scavassa@outlook.com>
2017-07-10 17:03:36 -0300

    Adding a new Controller to show how to treat actions in specific

---
+++ ExceptionFilter.API/Controllers/SpecificTreatedController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExceptionFilter.API.Filter;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExceptionFilter.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class SpecificTreatedController : Controller
+    {
+        // GET: api/values
+        [HttpGet("NoTreatment")]
+        public IEnumerable<string> NoTreatment()
+        {
+            throw  new Exception();
+        }
+
+        // GET api/values/5
+        [HttpGet("WithTreatment")]
+        [ExceptionHandlingFilter]
+        public string WithTreatment()
+        {
+            throw new Exception();
+        }
+        
+    }
+}
